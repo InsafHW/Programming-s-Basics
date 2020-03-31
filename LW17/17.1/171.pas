65cf4d69a4cf52f4adf0685500233de57ee2cd00
InsafHW <fororiginmylo@yandex.ru>
2020-03-31 20:22:53 +0300

    Works

--- LW17/17.1/171.pas
+++ LW17/17.1/171.pas
@@ -1,25 +1,25 @@
 PROGRAM Digits(INPUT, OUTPUT);
 VAR
   Digit, Sum: INTEGER;
-PROCEDURE ReadDigit(VAR CharDigit: TEXT; VAR IntDigit: INTEGER);
+PROCEDURE ReadDigit(VAR FileIn: TEXT; VAR Digit: INTEGER);
 VAR
   Ch: CHAR;
 BEGIN{ReadDigit}
   Digit := -1;
-  IF NOT EOLN(CharDigit)
+  IF NOT EOLN(FileIn)
   THEN
     BEGIN
-      READ(CharDigit, Ch);
-      IF (Ch = '0') THEN IntDigit := 0 ELSE
-      IF (Ch = '1') THEN IntDigit := 1 ELSE
-      IF (Ch = '2') THEN IntDigit := 2 ELSE
-      IF (Ch = '3') THEN IntDigit := 3 ELSE
-      IF (Ch = '4') THEN IntDigit := 4 ELSE
-      IF (Ch = '5') THEN IntDigit := 5 ELSE
-      IF (Ch = '6') THEN IntDigit := 6 ELSE
-      IF (Ch = '7') THEN IntDigit := 7 ELSE
-      IF (Ch = '8') THEN IntDigit := 8 ELSE
-      IF (Ch = '9') THEN IntDigit := 9
+      READ(FileIn, Ch);
+      IF (Ch = '0') THEN Digit := 0 ELSE
+      IF (Ch = '1') THEN Digit := 1 ELSE
+      IF (Ch = '2') THEN Digit := 2 ELSE
+      IF (Ch = '3') THEN Digit := 3 ELSE
+      IF (Ch = '4') THEN Digit := 4 ELSE
+      IF (Ch = '5') THEN Digit := 5 ELSE
+      IF (Ch = '6') THEN Digit := 6 ELSE
+      IF (Ch = '7') THEN Digit := 7 ELSE
+      IF (Ch = '8') THEN Digit := 8 ELSE
+      IF (Ch = '9') THEN Digit := 9
     END
 END;{ReadDigit}
 BEGIN{Digits}
@@ -31,9 +31,9 @@ BEGIN{Digits}
       Sum := Sum + Digit;
       ReadDigit(INPUT, Digit) 
     END;
-    IF (Sum = 0)
-    THEN
-      WRITELN('-1')
-    ELSE
-      WRITELN(Sum)
+  IF (Sum = 0)
+  THEN
+    WRITELN('-1')
+  ELSE
+    WRITELN(Sum)
 END.{Digits}
