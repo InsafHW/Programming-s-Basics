56c423bece62dcab4d0f23756921c0c46bd08496
InsafHW <fororiginmylo@yandex.ru>
2020-06-04 11:33:36 +0300

    lw20

--- LW20/20.1/201.pas
+++ LW20/20.1/201.pas
@@ -3,38 +3,40 @@ CONST
   Min = 1;
   Max = 25;
 TYPE
-  Position = SET OF Min .. Max;
+  Matrix  = SET OF Min .. Max;
 VAR
   I: INTEGER;
-  LetterPosition: Position;
-PROCEDURE PrintX(VAR F: TEXT; VAR Position: Position);
+  LetterPosition: Matrix;
+  
+PROCEDURE InitializeMatrix(VAR F: TEXT; VAR Matrix : Matrix );
 VAR
   Ch: CHAR;
 BEGIN{PrintX}
-  Position := [];
+  Matrix  := [];
   IF NOT EOLN(F)
   THEN
     BEGIN
       READ(F, Ch);
       IF (Ch = 'I')
       THEN
-        Position := [1, 2, 3, 4, 5, 8, 13, 18, 21, 22, 23, 24, 25]
+        Matrix  := [1, 2, 3, 4, 5, 8, 13, 18, 21, 22, 23, 24, 25]
       ELSE IF (Ch = 'N')
       THEN
-        Position := [1, 5, 6, 7, 10, 11, 13, 15, 16, 19, 20, 21, 25]
+        Matrix  := [1, 5, 6, 7, 10, 11, 13, 15, 16, 19, 20, 21, 25]
       ELSE IF (Ch = 'S')
       THEN
-        Position := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25]
+        Matrix  := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25]
       ELSE IF (Ch = 'A')
       THEN
-        Position := [2, 3, 4, 6, 10, 11, 12, 13, 14, 15, 16, 20, 21, 25]
+        Matrix  := [2, 3, 4, 6, 10, 11, 12, 13, 14, 15, 16, 20, 21, 25]
       ELSE IF (Ch = 'F')
       THEN
-        Position := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21];
+        Matrix  := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21];
     END;
 END;{PrintX}
+
 BEGIN{LetterX} 
-  PrintX(INPUT, LetterPosition);
+  InitializeMatrix(INPUT, LetterPosition);
   IF LetterPosition <> []
   THEN
     BEGIN
