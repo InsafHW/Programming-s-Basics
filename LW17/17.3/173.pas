5cc4b5e0ab87304dee84c85b2b87da70ff8de177
InsafHW <fororiginmylo@yandex.ru>
2020-06-21 20:48:04 +0300

    lw 17.3

--- LW17/17.3/173.pas
+++ LW17/17.3/173.pas
@@ -10,21 +10,17 @@ VAR
   Ch: CHAR;
 BEGIN{ReadDigit}
   Digit := -1;
-  IF NOT EOLN(FileIn)
-  THEN
-    BEGIN
-      READ(FileIn, Ch);
-      IF (Ch = '0') THEN Digit := 0 ELSE
-      IF (Ch = '1') THEN Digit := 1 ELSE
-      IF (Ch = '2') THEN Digit := 2 ELSE
-      IF (Ch = '3') THEN Digit := 3 ELSE
-      IF (Ch = '4') THEN Digit := 4 ELSE
-      IF (Ch = '5') THEN Digit := 5 ELSE
-      IF (Ch = '6') THEN Digit := 6 ELSE
-      IF (Ch = '7') THEN Digit := 7 ELSE
-      IF (Ch = '8') THEN Digit := 8 ELSE
-      IF (Ch = '9') THEN Digit := 9
-    END
+  READ(FileIn, Ch);
+  IF (Ch = '0') THEN Digit := 0 ELSE
+  IF (Ch = '1') THEN Digit := 1 ELSE
+  IF (Ch = '2') THEN Digit := 2 ELSE
+  IF (Ch = '3') THEN Digit := 3 ELSE
+  IF (Ch = '4') THEN Digit := 4 ELSE
+  IF (Ch = '5') THEN Digit := 5 ELSE
+  IF (Ch = '6') THEN Digit := 6 ELSE
+  IF (Ch = '7') THEN Digit := 7 ELSE
+  IF (Ch = '8') THEN Digit := 8 ELSE
+  IF (Ch = '9') THEN Digit := 9
 END;{ReadDigit}
 PROCEDURE ReadNumber(VAR FileIn: TEXT; VAR Number: INTEGER);
 CONST
