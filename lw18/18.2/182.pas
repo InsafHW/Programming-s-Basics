4a9e23629a8f6b7b481f2f75ebe78ab42160ccbb
InsafHW <fororiginmylo@yandex.ru>
2020-06-23 10:50:13 +0300

    18.2 is fixed

--- lw18/18.2/182.pas
+++ lw18/18.2/182.pas
@@ -10,31 +10,48 @@ VAR
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER;
   StudentName: TEXT;
-  Ch: CHAR;
- 
-PROCEDURE CopyName(VAR FIn: TEXT; VAR FOut: TEXT);
+  
+PROCEDURE ReadName(VAR FIn: TEXT; VAR FOut: TEXT);
 VAR
- Ch: CHAR;
-BEGIN
-  WHILE NOT EOLN(FIn)
+  Ch: CHAR;
+BEGIN{ReadName}
+  RESET(FIn);
+  IF NOT EOLN(FIn)
+  THEN
+    BEGIN
+      READ(FIn, Ch);
+      REWRITE(FOut);
+      WRITE(FOut, Ch)
+    END;
+  WHILE NOT EOLN(FIn) AND (Ch <> ' ')
   DO
     BEGIN
       READ(FIn, Ch);
       WRITE(FOut, Ch)
     END
-END;
+END;{ReadName}
 
-BEGIN{AverageScore}
+PROCEDURE WriteName(VAR FIn: TEXT; VAR FOut: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN{WriteName} 
+  RESET(FIn);
+  WHILE NOT EOLN(FIn) 
+  DO
+    BEGIN
+      READ(FIn, Ch);
+      WRITE(FOut, Ch)
+    END
+END;{WriteName}
+
+BEGIN{AverageScores}
   ClassTotal := 0;
   Student := 0;
+  WRITELN('Enter students names and their scores:');
   WHILE Student < ClassSize
   DO
     BEGIN
-      WRITE('Enter student name: ');
-      REWRITE(StudentName);
-      CopyName(INPUT, StudentName);
-      RESET(StudentName);
-      WRITE('Enter student scores: ');   
+      ReadName(INPUT, StudentName);
       TotalScore := 0;
       WhichScore := 0;
       WHILE WhichScore < NumberOfScores
@@ -43,28 +60,27 @@ BEGIN{AverageScore}
           READ(NextScore);
           IF (NextScore < 0) OR (NextScore > 100)
           THEN
-            WRITELN('Input error, score is out of border')
+            WRITE('Input error, score is out of border, write correct score: ')
           ELSE
             BEGIN
               TotalScore := TotalScore + NextScore;
               WhichScore := WhichScore + 1
             END
         END;
-      READLN;
-      TotalScore := TotalScore * 10;
-      Ave := TotalScore DIV NumberOfScores;
-      ClassTotal := ClassTotal + TotalScore;
-      Student := Student + 1;
-      RESET(StudentName);
-      CopyName(StudentName, OUTPUT);
-      IF Ave MOD 10 >= 5
-      THEN
-        WRITELN(' ', Ave DIV 10 + 1)
-      ELSE
-        WRITELN(' ', Ave DIV 10) 
+     READLN;
+     TotalScore := TotalScore * 10;
+     Ave := TotalScore DIV NumberOfScores;
+     ClassTotal := ClassTotal + TotalScore;
+     Student := Student + 1;
+     WriteName(StudentName, OUTPUT);
+     IF Ave MOD 10 >= 5
+     THEN
+       WRITELN(' ', Ave DIV 10 + 1)
+     ELSE
+       WRITELN(' ', Ave DIV 10)
     END;
   WRITELN;
   WRITELN ('Class average:');
   ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
   WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
-END.{AverageScore}
+END.  {AverageScore}
