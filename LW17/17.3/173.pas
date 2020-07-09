907f0d44f33802cf95681802c96675bef652bad4
InsafHW <fororiginmylo@yandex.ru>
2020-07-09 18:55:14 +0300

    17.3

--- LW17/17.3/173.pas
+++ LW17/17.3/173.pas
@@ -9,17 +9,21 @@ VAR
   Ch: CHAR;
 BEGIN{ReadDigit}
   Digit := -1;
-  READ(FileIn, Ch);
-  IF (Ch = '0') THEN Digit := 0 ELSE
-  IF (Ch = '1') THEN Digit := 1 ELSE
-  IF (Ch = '2') THEN Digit := 2 ELSE
-  IF (Ch = '3') THEN Digit := 3 ELSE
-  IF (Ch = '4') THEN Digit := 4 ELSE
-  IF (Ch = '5') THEN Digit := 5 ELSE
-  IF (Ch = '6') THEN Digit := 6 ELSE
-  IF (Ch = '7') THEN Digit := 7 ELSE
-  IF (Ch = '8') THEN Digit := 8 ELSE
-  IF (Ch = '9') THEN Digit := 9
+  IF NOT EOLN(FileIn)
+  THEN
+    BEGIN
+      READ(FileIn, Ch);
+      IF (Ch = '0') THEN Digit := 0 ELSE
+      IF (Ch = '1') THEN Digit := 1 ELSE
+      IF (Ch = '2') THEN Digit := 2 ELSE
+      IF (Ch = '3') THEN Digit := 3 ELSE
+      IF (Ch = '4') THEN Digit := 4 ELSE
+      IF (Ch = '5') THEN Digit := 5 ELSE
+      IF (Ch = '6') THEN Digit := 6 ELSE
+      IF (Ch = '7') THEN Digit := 7 ELSE
+      IF (Ch = '8') THEN Digit := 8 ELSE
+      IF (Ch = '9') THEN Digit := 9
+    END
 END;{ReadDigit}
 PROCEDURE ReadNumber(VAR FileIn: TEXT; VAR Number: INTEGER);
 VAR
@@ -56,7 +60,7 @@ BEGIN{ReadNumberProgram}
       IF (Number > Max)
       THEN
         Max := Number;
-      IF Sum < (MAXINT - Number)
+      IF Sum <= (MAXINT - Number)
       THEN
         BEGIN
           Sum := Sum + Number;
